cefc80da770f7265fa50122f8f7371948e37e5d6
Leila.L <leila.lababsa@hotmail.fr>
2017-11-29 09:25:33 +0100

    header+footer html done

--- views/template/footer.php
+++ views/template/footer.php
@@ -1,12 +1,46 @@
-<footer class="jumbotron jumbotron-fluid mt-5 mainColor colorWhite">
+<footer id="footer" class="page-footer">
   <div class="container">
-    <h3>Made in Simplon</h3>
+    <div class="row">
+      <div class="col l12 s12">
+        <h5 class="white-text ">Footer</h5>
+      </div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container left-align">
+      © 2017 Copyright Leila
+      <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+    </div>
   </div>
 </footer>
 
 
-<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+
+<script src="../assets/js/vendor/modernizr-2.8.3.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+<script>window.jQuery || document.write('<script src="../assets/js/vendor/jquery-1.12.0.min.js"><\/script>')</script>
+<script src="../assets/js/plugins.js"></script>
+<script src="../assets/js/materialize.js"></script>
+<script src="../assets/js/main.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.1/js/materialize.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+  $('.modal').modal();
+});
+</script>
+
+
+
+
+<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+<script>
+(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+  function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+  e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+  e.src='https://www.google-analytics.com/analytics.js';
+  r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+  ga('create','UA-XXXXX-X','auto');ga('send','pageview');
+  </script>
 </body>
 </html>
--- views/template/header.php
+++ views/template/header.php
@@ -1,15 +1,29 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title>Your site title</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    <link rel="stylesheet" href="../assets/css/main.css">
-  </head>
-  <body>
-    <header class="jumbotron jumbotron-fluid mb-5 mainColor colorWhite">
-      <div class="container">
-        <h1 class="display-3">L'héritage en PHP</h1>
-        <p class="lead">atelier Simplon</p>
+<head>
+  <meta charset="utf-8">
+  <title>Library</title>
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="icon" href="../favicon.ico">
+  <link rel="stylesheet" href="../assets/css/materialize.css">
+  <link rel="stylesheet" href="../assets/css/normalize.css">
+  <link rel="stylesheet" href="../assets/css/main.css">
+  <link rel="stylesheet" href="../assets/css/style.css">
+</head>
+<body>
+  <header >
+
+    <nav id="header">
+      <div class="nav-wrapper center-align">
+        <a href="../controllers/index.php" class="brand-logo">Bank_Able</a>
       </div>
-    </header>
+    </nav>
+
+  </header>
+</body>
